6b53a49738f1f9fe9a434aa7e50bcd9616d1623a
reneMX <rene.martinez.magana@estudiante.uacm.edu.mx>
2020-10-31 00:58:15 -0600

    Version 0.0.1

--- Tokens.py
+++ Tokens.py
@@ -30,117 +30,64 @@ class Lexer():
     def getX(self):
         return self.x
 
-
-    def obtenIdentificadores(self):
+    def obtenPractica3(self):
         self.file = open('Area.c', mode='r')
-        #Leemos todas las lineas del archivo
+        # Leemos todas las lineas del archivo
         archivo = self.file.read()
+
+        '''Reconocimiento de Identificadores'''
         print("BUSQUEDA DE IDENTIFICADORES")
-        #V1
-        # x = re.findall("[^\d+.\d{1,2}|^#|^%]*",archivo)
-        #V2
-        #self.x = re.findall("[^//a-z]+[_a-zA-Z][_a-zA-Z0-9 ]{0,30}[^(\"\/nA-Za-z \")+]", archivo)
-        #V3
         self.list_identi = re.findall(self.exp_identificadores, archivo)
         print(self.list_identi)
-        self.file.close()
 
-
-    def obtenEnteros(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+        '''Reconocimiento de Enteros'''
         print "BUSQUEDA DE ENTEROS"
-        #VR1
-        #self.x = re.findall("[\d][^*|^.\d[f|d|i|c|s]]",archivo)
-        #VR2
-        self.x = re.findall("[+|-][\d]+|[\d]+",archivo)
+        self.x = re.findall("[+|-][\d]+|[\d]+", archivo)
         print(self.x)
-        self.file.close()
 
-    def obtenReales(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+        '''Reconocimiento de Reales'''
         print "BUSQUEDA DE REALES"
-        #VR1
-        #self.x = re.findall("[\d]+[.][\d]*",archivo)
-        #VR2
-        self.x = re.findall("[\d]+[.][\d]*|[-|+]+[.][\d]*|[-|+]*[.][\d]+|[.][\d]+",archivo)
+        self.x = re.findall("[\d]+[.][\d]*|[-|+]+[.][\d]*|[-|+]*[.][\d]+|[.][\d]+", archivo)
         print(self.x)
 
-    def obtenComentarios(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+        '''Reconocimiento de comentarios'''
         print "BUSQUEDA DE COMENTARIOS"
-        #VR!
-        #self.x = re.findall("//[A-Za-z ]*",archivo)
-
-        #VR2
         self.x = re.findall("[/][/|*][a-z |A-Za-z]*[ |*/]*", archivo)
         print(self.x)
 
-
-    def obtenAritmeticos(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+        '''Operadores Aritmeticos'''
         print "BUSQUEDA DE OPERADORES ARITMETICOS"
         self.x = re.findall("[+|-|*|=|/|%]", archivo)
         print(self.x)
 
-    def obtenLogicos(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+        '''Operadores Logicos'''
         print "BUSQUEDA DE OPERADORES LOGICOS"
         self.x = re.findall("[&&|||>|<|>=|<=]+|/", archivo)
         print(self.x)
-    def obtenFor(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+
+        '''Obtener For'''
         self.x = re.findall("for", archivo)
         print "For's"
         print(self.x)
 
-    def obtenIf(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+        '''Obtener IF'''
+        print "Obten IF's"
         self.x = re.findall("if", archivo)
-        print "IF's"
         print(self.x)
-    def obtenSaltos(self):
-        self.file = open('Area.c', mode='r')
-        archivo = self.file.read()
+
+        '''Obtener Saltos'''
         self.x = re.findall("\n+", archivo)
-        print "SAltos de Linea"
+        print "Saltos de Linea"
         print(len(self.x))
 
-
+        #Cerramos el archivo
+        self.file.close()
+        #FIN DE LA FUNCION obtenPractica3()
 
 """INSTANCIAS"""
 
 l = Lexer()
+l.obtenPractica3()
 
-'''Reconocimiento de Identificadores'''
-#l.obtenIdentificadores()
-
-'''Reconocimiento de Enteros'''
-#l.obtenEnteros()
-
-'''Reconocimiento de Reales'''
-#l.obtenReales()
-
-'''Reconocimiento de comentarios'''
-#l.obtenComentarios()
-
-'''Operadores Aritmeticos'''
-#l.obtenAritmeticos()
-
-'''Operadores Logicos'''
-#l.obtenLogicos()
-
-'''Obtener For'''
-l.obtenFor()
-'''Obtener IF'''
-l.obtenIf()
-'''Obtener Saltos'''
-l.obtenSaltos()
 
 
